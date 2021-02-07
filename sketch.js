152df91b789ec12f39e92663a595083884b6b7fd
ParthGoyal08 <69162910+ParthGoyal08@users.noreply.github.com>
2021-02-07 12:13:59 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ function preload(){
   
 bg = loadImage("jungle.png");
 
-monkey = loadAnimation("monkey_01.png,monkey_02.png,monkey_03.png,monkey_04.png,monkey_05.png,monkey_06.png,monkey_07.png,monkey_08.png,monkey_09.png,monkey_10.png");
+monkey = loadAnimation("monkey_01.png","monkey_02.png","monkey_03.png","monkey_04.png","monkey_05.png","monkey_06.png","monkey_07.png","monkey_08.png","monkey_09.png","monkey_10.png");
   
 bananaImage = loadImage("banana.png");
 obstacleImage = loadImage("stone.png");  
